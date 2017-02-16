maidsafe/sn_routing

fix/hop signatures: never try to verify paths for messages from clients, better diagnostics

src/states/client.rs
@@ -174,9 +174,17 @@ impl Client {
         }
 
         let signed_msg = hop_msg.content;
-        // TODO: verify with proxy's section list
-        try_ev!(signed_msg.check_integrity(self.min_group_size(), None),
-                result);
+        // TODO: verify with proxy's section list instead of passing None here:
+        match signed_msg.check_integrity(self.min_group_size(), None) {
+            Ok(()) => {}
+            Err(e) => {
+                warn!("{:?} Verification of {:?} failed: {:?}",
+                      self,
+                      signed_msg,
+                      e);
+                return result.with_value(Err(e.into()));
+            }
+        }
 
         let routing_msg = signed_msg.routing_message();
         let in_authority = self.in_authority(&routing_msg.dst);

src/states/node.rs
@@ -678,18 +678,30 @@ impl Node {
             .routing_table()
             .find_group_prefix(&hop_name)
             .ok_or(RoutingTableError::NoSuchPeer), result);
-        let section_list = self.section_list_sigs.get_signed_list(&hop_prefix);
-        if section_list.is_none() && !signed_msg.routing_message().src.is_client() {
-            warn!("NoSecSigInCache: sender {:?} of signed message {:?} to {:?} via hop {:?} \
-                cannot be verified",
-                signed_msg.routing_message().src,
-                signed_msg.routing_message().content,
-                signed_msg.routing_message().dst, hop_name);
-        }
+        let section_list = if signed_msg.routing_message().src.is_client() {
+            None
+        } else {
+            let list = self.section_list_sigs.get_signed_list(&hop_prefix);
+            if list.is_none() {
+                warn!("{:?} NoSecSigInCache: sender {:?} of signed message {:?} to {:?} \
+                        via hop {:?} cannot be verified",
+                    self,
+                    signed_msg.routing_message().src,
+                    signed_msg.routing_message().content,
+                    signed_msg.routing_message().dst, hop_name);
+            }
+            list
+        };
 
         // Check that content signatures, and (if we have a section list) the sender.
-        try_ev!(signed_msg.check_integrity(self.min_group_size(),
-                section_list.as_ref().map(|sl| &sl.list)), result);
+        match signed_msg.check_integrity(self.min_group_size(),
+                                         section_list.as_ref().map(|sl| &sl.list)) {
+            Ok(()) => {}
+            Err(e) => {
+                warn!("{:?} Verification of {:?} failed: {:?}", self, signed_msg, e);
+                return result.with_value(Err(e.into()));
+            }
+        }
 
         match self.routing_msg_filter.filter_incoming(signed_msg.routing_message(), route) {
             FilteringResult::KnownMessageAndRoute => {
@@ -727,8 +739,9 @@ impl Node {
             return result.map(Ok);
         }
 
-        let section_list = try_ev!(section_list.ok_or(RoutingTableError::NoSecSigInCache), result);
-        signed_msg.add_relaying_section(section_list);
+        if let Some(list) = section_list {
+            signed_msg.add_relaying_section(list);
+        }
         if let Err(error) = self.send_signed_message(&signed_msg, route, &hop_name, sent_to)
             .extract(&mut result) {
             debug!("{:?} Failed to send {:?}: {:?}", self, signed_msg, error);

michaelsproul/routing

src/states/client.rs
@@ -177,8 +177,17 @@ impl Client {
         }
 
         let signed_msg = hop_msg.content;
-        // TODO: verify with proxy's section list
-        signed_msg.check_integrity(self.min_section_size(), None)?;
+        // TODO: verify with proxy's section list instead of passing None here:
+        match signed_msg.check_integrity(self.min_section_size(), None) {
+            Ok(()) => {}
+            Err(e) => {
+                warn!("{:?} Verification of {:?} failed: {:?}",
+                      self,
+                      signed_msg,
+                      e);
+                return Err(e.into());
+            }
+        }
 
         let routing_msg = signed_msg.routing_message();
         let in_authority = self.in_authority(&routing_msg.dst);

src/states/node.rs
@@ -762,19 +762,34 @@ impl Node {
             .routing_table()
             .find_section_prefix(&hop_name)
             .ok_or(RoutingTableError::NoSuchPeer)?;
-        let section_list = self.section_list_sigs.get_signed_list(&hop_prefix);
-        if section_list.is_none() && !signed_msg.routing_message().src.is_client() {
-            warn!("NoSecSigInCache: sender {:?} of signed message {:?} to {:?} via hop {:?} \
-                cannot be verified",
-                  signed_msg.routing_message().src,
-                  signed_msg.routing_message().content,
-                  signed_msg.routing_message().dst,
-                  hop_name);
-        }
+        let section_list = if signed_msg.routing_message().src.is_client() {
+            None
+        } else {
+            let list = self.section_list_sigs.get_signed_list(&hop_prefix);
+            if list.is_none() {
+                warn!("{:?} NoSecSigInCache: sender {:?} of signed message {:?} to {:?} \
+                        via hop {:?} cannot be verified",
+                      self,
+                      signed_msg.routing_message().src,
+                      signed_msg.routing_message().content,
+                      signed_msg.routing_message().dst,
+                      hop_name);
+            }
+            list
+        };
 
         // Check that content signatures, and (if we have a section list) the sender.
-        signed_msg.check_integrity(self.min_section_size(),
-                             section_list.as_ref().map(|sl| &sl.list))?;
+        match signed_msg.check_integrity(self.min_section_size(),
+                                         section_list.as_ref().map(|sl| &sl.list)) {
+            Ok(()) => {}
+            Err(e) => {
+                warn!("{:?} Verification of {:?} failed: {:?}",
+                      self,
+                      signed_msg,
+                      e);
+                return Err(e.into());
+            }
+        }
 
         match self.routing_msg_filter.filter_incoming(signed_msg.routing_message(), route) {
             FilteringResult::KnownMessageAndRoute => {
@@ -797,8 +812,9 @@ impl Node {
             return Ok(());
         }
 
-        let section_list = section_list.ok_or(RoutingTableError::NoSecSigInCache)?;
-        signed_msg.add_relaying_section(section_list);
+        if let Some(list) = section_list {
+            signed_msg.add_relaying_section(list);
+        }
         if let Err(error) = self.send_signed_message(signed_msg, route, &hop_name, sent_to) {
             debug!("{:?} Failed to send signed message: {:?}", self, error);
         }