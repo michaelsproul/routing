maidsafe/sn_routing

feat/messages: Check hop path in SignedMessage::check_integrity
Exception: clients can't do this yet
Exception: tests don't use it yet

src/messages.rs
@@ -261,10 +261,63 @@ impl SignedMessage {
     }
 
     /// Confirms the signatures.
-    // TODO (1677): verify the sending SectionLists via each hop's signed lists
-    pub fn check_integrity(&self, min_group_size: usize) -> Result<(), RoutingError> {
-        // TODO: verify relay and sending section lists
+    ///
+    /// `min_group_size` is required when the message is from a _group_ (see doc on `Authority`).
+    ///
+    /// `known_section` is used to kick-start hop-by-hop verification. It is required to verify
+    /// the source section/group; it should be mandatory but currently clients don't know who
+    /// the members of their proxy's section are (TODO).
+    pub fn check_integrity(&self,
+                           min_group_size: usize,
+                           known_section: Option<&SectionList>)
+                           -> Result<(), RoutingError> {
+        fn check_enough_sigs(ssl: &SignedSectionList,
+                             last_verified: &SectionList)
+                             -> Result<(), RoutingError> {
+            let bytes = serialise(&ssl.list)?;
+            // Now find all signatures of bytes from ssl which are valid and from a node
+            // mentioned by last_verified, and check that this is a quorum of last_verified.
+            let valid_sigs = ssl.signatures
+                .iter()
+                .filter(|&(pub_id, sig): &(&PublicId, &sign::Signature)| {
+                    last_verified.pub_ids.contains(pub_id) &&
+                    sign::verify_detached(sig, &bytes, pub_id.signing_public_key())
+                })
+                .count();
+            if QUORUM * last_verified.pub_ids.len() > 100 * valid_sigs {
+                return Err(RoutingError::NotEnoughSignatures);
+            }
+            Ok(())
+        }
 
+        // -----  first, verify the route  -----
+        if let Some(known_section) = known_section {
+            let mut last_verified = known_section;
+            for prev_hop in self.relay_sections.iter().rev() {
+                check_enough_sigs(prev_hop, last_verified)?;
+                last_verified = &prev_hop.list;
+            }
+
+            // Now known_section should correspond to the accumulating section. We could directly
+            // use this to verify all source sections, but the accumulating node's signed lists of
+            // these sections may only include a quorum of signatures from its own understanding
+            // of its section, so we resolve that and use that to verify the rest.
+            for src in &self.src_sections {
+                if src.list.prefix == last_verified.prefix {
+                    check_enough_sigs(src, last_verified)?;
+                    last_verified = &src.list;
+                }
+            }
+            for src in &self.src_sections {
+                if src.list.prefix == last_verified.prefix {
+                    // we already verified this section list
+                    continue;
+                }
+                check_enough_sigs(src, last_verified)?;
+            }
+        }
+
+        // -----  second, verify the message was signed by a quorum of original senders  -----
         let signed_bytes = serialise(&self.content)?;
         if !self.find_invalid_sigs(signed_bytes).is_empty() {
             return Err(RoutingError::FailedSignature);
@@ -320,9 +373,6 @@ impl SignedMessage {
         // We also check (again) that all messages are from valid senders, because the message
         // may have been sent from another node, and we cannot trust that that node correctly
         // controlled which signatures were added.
-        // TODO (1677): we also need to check that the src_sections list corresponds to the
-        // section(s) at some point in recent history; i.e. that it was valid; but we shouldn't
-        // force it to match our own because our routing table may have changed since.
 
         let signed_bytes = match serialise(&self.content) {
             Ok(serialised) => serialised,
@@ -368,8 +418,10 @@ impl SignedMessage {
         invalid
     }
 
-    // Returns true iff there are enough signatures (note that this method does not verify the
-    // signatures, it only counts them; it also does not verify `self.src_sections`).
+    // Returns true iff there are a quorum of signatures from `self.src_sections`.
+    //
+    // Note that this method does not verify the signatures, it only counts them.
+    // It also does not verify anything about `self.src_sections`.
     fn has_enough_sigs(&self, min_group_size: usize) -> bool {
         use Authority::*;
         match self.content.src {
@@ -1122,6 +1174,8 @@ mod tests {
 
     #[test]
     fn signed_message_check_integrity() {
+        // TODO: update to test the new route and sender the validation in check_integrity
+
         let min_group_size = 1000;
         let name: XorName = rand::random();
         let full_id = FullId::new();
@@ -1144,7 +1198,7 @@ mod tests {
         assert_eq!(Some(full_id.public_id()),
                    signed_message.signatures.keys().next());
 
-        unwrap!(signed_message.check_integrity(min_group_size));
+        unwrap!(signed_message.check_integrity(min_group_size, None));
 
         let full_id = FullId::new();
         let bytes_to_sign = unwrap!(serialise(&(&routing_message, full_id.public_id())));
@@ -1153,7 +1207,7 @@ mod tests {
         signed_message.signatures = iter::once((*full_id.public_id(), signature)).collect();
 
         // Invalid because it's not signed by the sender:
-        assert!(signed_message.check_integrity(min_group_size).is_err());
+        assert!(signed_message.check_integrity(min_group_size, None).is_err());
         // However, the signature itself should be valid:
         assert!(signed_message.has_enough_sigs(min_group_size));
     }

michaelsproul/routing

src/messages.rs
@@ -301,10 +301,63 @@ impl SignedMessage {
     }
 
     /// Confirms the signatures.
-    // TODO (1677): verify the sending SectionLists via each hop's signed lists
-    pub fn check_integrity(&self, min_section_size: usize) -> Result<(), RoutingError> {
-        // TODO: verify relay and sending section lists
+    ///
+    /// `min_section_size` is required when the message is from a _group_ (see doc on `Authority`).
+    ///
+    /// `known_section` is used to kick-start hop-by-hop verification. It is required to verify
+    /// the source section/group; it should be mandatory but currently clients don't know who
+    /// the members of their proxy's section are (TODO).
+    pub fn check_integrity(&self,
+                           min_section_size: usize,
+                           known_section: Option<&SectionList>)
+                           -> Result<(), RoutingError> {
+        fn check_enough_sigs(ssl: &SignedSectionList,
+                             last_verified: &SectionList)
+                             -> Result<(), RoutingError> {
+            let bytes = serialise(&ssl.list)?;
+            // Now find all signatures of bytes from ssl which are valid and from a node
+            // mentioned by last_verified, and check that this is a quorum of last_verified.
+            let valid_sigs = ssl.signatures
+                .iter()
+                .filter(|&(pub_id, sig): &(&PublicId, &sign::Signature)| {
+                    last_verified.pub_ids.contains(pub_id) &&
+                    sign::verify_detached(sig, &bytes, pub_id.signing_public_key())
+                })
+                .count();
+            if QUORUM * last_verified.pub_ids.len() > 100 * valid_sigs {
+                return Err(RoutingError::NotEnoughSignatures);
+            }
+            Ok(())
+        }
 
+        // -----  first, verify the route  -----
+        if let Some(known_section) = known_section {
+            let mut last_verified = known_section;
+            for prev_hop in self.relay_sections.iter().rev() {
+                check_enough_sigs(prev_hop, last_verified)?;
+                last_verified = &prev_hop.list;
+            }
+
+            // Now known_section should correspond to the accumulating section. We could directly
+            // use this to verify all source sections, but the accumulating node's signed lists of
+            // these sections may only include a quorum of signatures from its own understanding
+            // of its section, so we resolve that and use that to verify the rest.
+            for src in &self.src_sections {
+                if src.list.prefix == last_verified.prefix {
+                    check_enough_sigs(src, last_verified)?;
+                    last_verified = &src.list;
+                }
+            }
+            for src in &self.src_sections {
+                if src.list.prefix == last_verified.prefix {
+                    // we already verified this section list
+                    continue;
+                }
+                check_enough_sigs(src, last_verified)?;
+            }
+        }
+
+        // -----  second, verify the message was signed by a quorum of original senders  -----
         let signed_bytes = serialise(&self.content)?;
         if !self.find_invalid_sigs(signed_bytes).is_empty() {
             return Err(RoutingError::FailedSignature);
@@ -360,9 +413,6 @@ impl SignedMessage {
         // We also check (again) that all messages are from valid senders, because the message
         // may have been sent from another node, and we cannot trust that that node correctly
         // controlled which signatures were added.
-        // TODO (1677): we also need to check that the src_sections list corresponds to the
-        // section(s) at some point in recent history; i.e. that it was valid; but we shouldn't
-        // force it to match our own because our routing table may have changed since.
 
         let signed_bytes = match serialise(&self.content) {
             Ok(serialised) => serialised,
@@ -406,8 +456,10 @@ impl SignedMessage {
         invalid
     }
 
-    // Returns true iff there are enough signatures (note that this method does not verify the
-    // signatures, it only counts them; it also does not verify `self.src_sections`).
+    // Returns true iff there are a quorum of signatures from `self.src_sections`.
+    //
+    // Note that this method does not verify the signatures, it only counts them.
+    // It also does not verify anything about `self.src_sections`.
     fn has_enough_sigs(&self, min_section_size: usize) -> bool {
         use Authority::*;
         match self.content.src {
@@ -1234,6 +1286,8 @@ mod tests {
 
     #[test]
     fn signed_message_check_integrity() {
+        // TODO: update to test the new route and sender the validation in check_integrity
+
         let min_section_size = 1000;
         let name: XorName = rand::random();
         let full_id = FullId::new();
@@ -1256,7 +1310,7 @@ mod tests {
         assert_eq!(Some(full_id.public_id()),
                    signed_message.signatures.keys().next());
 
-        unwrap!(signed_message.check_integrity(min_section_size));
+        unwrap!(signed_message.check_integrity(min_section_size, None));
 
         let full_id = FullId::new();
         let bytes_to_sign = unwrap!(serialise(&(&routing_message, full_id.public_id())));
@@ -1265,7 +1319,7 @@ mod tests {
         signed_message.signatures = iter::once((*full_id.public_id(), signature)).collect();
 
         // Invalid because it's not signed by the sender:
-        assert!(signed_message.check_integrity(min_section_size).is_err());
+        assert!(signed_message.check_integrity(min_section_size, None).is_err());
         // However, the signature itself should be valid:
         assert!(signed_message.has_enough_sigs(min_section_size));
     }